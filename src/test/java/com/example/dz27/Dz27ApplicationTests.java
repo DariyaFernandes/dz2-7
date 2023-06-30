26ae1320691105f10664981609cf1b16eae49b2f
ааа <https://github.com/@DariyaFernandes/Homework1/pull/1>
2023-06-30 09:23:31 +0300

    dz2-7

---
+++ src/test/java/com/example/dz27/Dz27ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dz27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dz27ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
